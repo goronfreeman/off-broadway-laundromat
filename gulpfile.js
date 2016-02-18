abc6057f626feb2ab695b574d2c56465890b664e
Hunter Braun <hunter.braun@gmail.com>
2016-02-18 03:00:06 -0600

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,63 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync');
-var cp          = require('child_process');
+var gulp         = require('gulp'),
+    browserSync  = require('browser-sync'),
+    cp           = require('child_process'),
+    concat       = require('gulp-concat'),
+    rename       = require('gulp-rename'),
+    uglify       = require('gulp-uglify'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concatCss    = require('gulp-concat-css'),
+    cssnano      = require('gulp-cssnano'),
+    sass         = require('gulp-sass'),
+    imagemin     = require('gulp-imagemin');
 
 var messages = {
   jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
 };
 
-// Build the Jekyll site
+gulp.task('sass', function() {
+  return gulp.src('assets/css/main.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('assets/css'));
+});
+
+// Autoprefix, Concat, & Minify CSS
+gulp.task('css', function() {
+  return gulp.src('assets/css/main.css')
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false
+  }))
+  .pipe(cssnano())
+  .pipe(gulp.dest('assets/css'))
+  .pipe(rename('main.min.css'))
+  .pipe(gulp.dest('assets/css'))
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+  return gulp.src('assets/js/*.js')
+  .pipe(concat('main.js'))
+  .pipe(gulp.dest('assets/js'))
+  .pipe(rename('main.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('assets/js'));
+});
+
+// Minify Images
+gulp.task('images', function() {
+  return gulp.src('assets/img/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('assets/img'));
+});
+
+// Build the Jekyll Site
 gulp.task('jekyll-build', function(done) {
   browserSync.notify(messages.jekyllBuild);
   return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
     .on('close', done);
 });
 
-// Rebuild Jekyll & do page reload
+// Rebuild Jekyll & Reload Page
 gulp.task('jekyll-rebuild', ['jekyll-build'], function() {
   browserSync.reload();
 });
@@ -27,10 +71,12 @@ gulp.task('browser-sync', ['jekyll-build'], function() {
   });
 });
 
-// Watch html/md files, run Jekyll & reload BrowserSync
+// Watch Files for Changes
 gulp.task('watch', function() {
-  gulp.watch(['./*', '_layouts/*', '_includes/*', '_posts/*', '_sass/*', 'css/*'], ['jekyll-rebuild']);
+  gulp.watch('assets/css/*.scss', ['sass', 'css']);
+  gulp.watch('assets/js/*.js', ['scripts']);
+  gulp.watch(['index.html', '_config.yml', '_layouts/*', '_includes/*', '_posts/*', '_assets/*'], ['jekyll-rebuild']);
 });
 
-// Default task, running just `gulp` will compile the sass, compile the Jekyll site, launch BrowserSync & watch files.
-gulp.task('default', ['browser-sync', 'watch']);
+// Default Task
+gulp.task('default', ['sass', 'css', 'scripts', 'images', 'browser-sync', 'watch']);
